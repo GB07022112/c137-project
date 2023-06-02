428aee0ed0f2009188b4c1c79b9feda8a618e95f
GB07022112 <128438908+GB07022112@users.noreply.github.com>
2023-06-02 15:56:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status = "";
+
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380, 380);
+  video.hide();
+}
+function start(){
+  objectFinder = ml5.objectFinder('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function draw()
+{
+    video.live();
+}
